bd41586c18220e2f4bdffbbbb59602ad939aea8d
Alexander Koller <akoller@gmail.com>
2025-07-21 14:46:49 +0200

    prettier traces

--- tracing.py
+++ tracing.py
@@ -11,18 +11,26 @@ class Trace:
         raise NotImplementedError
 
 
+
+######## Write trace to a plain text file ##############
+
 class TraceToFile(Trace):
     def __init__(self, filename):
         self.file = open(filename, 'w')
+        self.first = True
 
     def accept_tokens(self, tokens):
+        if not self.first:
+            self.file.write("\n----------------------\n\n")
+
         self.file.write(f"{tokens}\n\n")
+        self.first = False
 
     def accept(self, variable, value):
         self.file.write(f"{variable}\n{value}\n\n")
 
     def accept_result(self, result):
-        self.file.write(f"{result}\n")
+        self.file.write(f"Result: {result}\n")
 
     def close(self):
         if not self.file.closed:
@@ -36,25 +44,68 @@ class TraceToFile(Trace):
         self.close()
 
 
+
+
+
+######## Write trace to an HTML file containing a table ##############
+
+html_header = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>C-RASP trace</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      border: 1px solid black;
+      margin-bottom:50px;
+    }
+
+    th {
+      padding: 12px;
+      background-color: #eeeeee;
+      font-weight: bold;
+      border: 1px solid black;
+      text-align: left;
+    }
+    
+    td {
+      padding: 12px;
+      border: 1px solid black;
+    }
+    
+    .final {
+        background-color: #eeeeee;
+        font-weight: bold;
+    }
+  </style>
+</head>
+<body>
+<table>
+'''
+
 class TraceToHTML(Trace):
     num_tokens = 0
     first = True
 
     def __init__(self, filename):
         self.file = open(filename, 'w')
-        self.file.write("<table border='1'>\n")
+        self.file.write(html_header)
 
-    def write_list(self, list, markup=lambda x: x):
+    def write_list(self, list, markup=lambda x: x, element="td", elclass=None):
         self.file.write("<tr>\n")
+        str_elclass = f" class='{elclass}'" if elclass else ""
         for val in list:
-            self.file.write("<td style='padding: 10px;'>{}</td>\n".format(markup(val)))
+            self.file.write(f"<{element}{str_elclass}>{markup(val)}</{element}>\n")
         self.file.write("</tr>\n")
 
     def accept_tokens(self, tokens):
         if not self.first:
-            self.file.write("</table><table border='1'>\n")
+            self.file.write("</table><table>\n")
 
-        self.write_list([""] + list(tokens))
+        self.write_list([""] + list(tokens), element="th")
         self.num_tokens = len(tokens)
         self.first = False
 
@@ -62,11 +113,11 @@ class TraceToHTML(Trace):
         self.write_list([variable] + value)
 
     def accept_result(self, result):
-        self.write_list(["Result"] + [""] * (self.num_tokens-1) + [result], markup=lambda x: f"<strong>{x}</strong>")
+        self.write_list(["Result"] + [""]*(self.num_tokens-1) + [result], elclass="final")
 
     def close(self):
         if not self.file.closed:
-            self.file.write("</table>\n")
+            self.file.write("</table></body></html>\n")
             self.file.flush()
             self.file.close()
 
