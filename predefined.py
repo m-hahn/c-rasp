9e8230232bae6557298a7993be96517f6b44ef88
Alexander Koller <akoller@gmail.com>
2025-07-21 14:13:02 +0200

    explanation in predefined.py

--- predefined.py
+++ predefined.py
@@ -1,12 +1,17 @@
 
-
-# All input positions are zero-based.
+# Implement unary and binary predicates as Python functions in this file.
 
 # For the binary predicates, i and j are as in Huang et al. 2025:
 # i is the position at which we're evaluating #[predicate], and j is the position to the left
 # at which we're evaluating the nested expression.
 
-# Binary predicates must be translation-invariant; unary predicates must be local.
+# You can define predicates as you wish. Huang et al. 2025 are focused
+# on binary predicates that are translation-invariant and local and on unary predicates
+# that are periodic. Such C-RASP programs can be compiled into length-generalizing
+# limit transformers.
+
+# All input positions are zero-based.
+
 
 def pred(i: int, j: int) -> bool:
     return j == i-1
